8a2459ee005ed9878e2beb32b2d09b39902d85de
Robert Anderson <35211428+robertanderson1553@users.noreply.github.com>
2018-01-25 23:27:40 -0800

    updates to project

---
+++ ProjectPhase02/Anderson_Robert/Anderson_Robert/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Anderson_Robert.Models;
+using Microsoft.Extensions.Configuration;
+using Anderson_Robert.DAL;
+
+namespace Anderson_Robert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page2(Person person)
+        {
+            // send it to the DB
+            // do some validation
+
+            //string connStr = configuration.GetConnectionString("MyConnStr");
+
+            DALPerson dp = new DALPerson(this.configuration);
+            string uID = dp.AddPerson(person);
+
+            person.PersonID = uID;
+
+            return View(person);
+        }
+
+
+    }
+}
