c8e2ca7122bc24471158d810b584b15e8275d6f8
Robert Anderson <35211428+robertanderson1553@users.noreply.github.com>
2018-02-19 21:40:41 -0800

    Added Update Database
    Added Project Phase 4 - Update Database

---
+++ ProjectPhase04/Anderson_Robert/Anderson_Robert/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anderson_Robert.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
